42287fc1168111512055c5c33c44f992938676a4
Brodoloeins <joaofernandovascao@gmail.com>
2024-02-07 21:09:17 -0300

    Aula 03

--- src/http/server.ts
+++ src/http/server.ts
@@ -4,6 +4,8 @@ import { createPoll } from "./routes/createpoll";
 import { getPoll } from "./routes/getpoll";
 import { voteOnPoll } from "./routes/voteonpoll";
 import cookie from "@fastify/cookie";
+import websocket from "@fastify/websocket";
+import { pollResults } from "./ws/poll-results";
 
 const app = fastify()
 
@@ -12,12 +14,14 @@ app.register(cookie, {
     hook: 'onRequest',
     parseOptions: {}
 })
+app.register(websocket)
+
 
 app.register(createPoll)
-
 app.register(getPoll)
-
 app.register(voteOnPoll)
+app.register(pollResults)
+
 
 app.listen({port: 3333}).then(() => {
     console.log('Http server listening on port 3333')
